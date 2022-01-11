2e2daecde82873e57c1a063012bf95565f6e66d3
Bartek_Praca <bartekj141@wp.pl>
2022-01-11 18:30:57 +0100

    initial git

---
+++ src/main/java/com/jakubartlomiej/findyourfriends/FindYourFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.jakubartlomiej.findyourfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubartlomiej/findyourfriends/FindYourFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubartlomiej.findyourfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
