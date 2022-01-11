1d8fbee22ce9430e53a37c60fa2e6601a3d57832
Bartek_Praca <bartekj141@wp.pl>
2022-01-11 20:19:06 +0100

    add repository user and role

--- src/main/java/com/jakubartlomiej/findyourfriends/user/repository/RoleRepository.java
+++ src/main/java/com/jakubartlomiej/findyourfriends/user/repository/RoleRepository.java
@@ -1,2 +1,10 @@
-package com.jakubartlomiej.findyourfriends.user.repository;public interface RoleRepository {
+package com.jakubartlomiej.findyourfriends.user.repository;
+
+import com.jakubartlomiej.findyourfriends.user.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
