177f1b2d168fc137f63999a5369639adc28cb94d
Bartek_Praca <bartekj141@wp.pl>
2022-01-11 20:20:05 +0100

    added service user and role

--- src/main/java/com/jakubartlomiej/findyourfriends/user/service/UserService.java
+++ src/main/java/com/jakubartlomiej/findyourfriends/user/service/UserService.java
@@ -1,2 +1,26 @@
-package com.jakubartlomiej.findyourfriends.user.service;public class UserService {
+package com.jakubartlomiej.findyourfriends.user.service;
+
+import com.jakubartlomiej.findyourfriends.user.domain.User;
+import com.jakubartlomiej.findyourfriends.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void disabledAccount(User user) {
+        User currentUser = userRepository.findByUsername(user.getUsername());
+        currentUser.setEnabled(false);
+        save(user);
+    }
 }
